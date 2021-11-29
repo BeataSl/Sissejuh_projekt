e3e8af2bd05770920741bbd453d4a1f9900592b2
Namsua <93035882+Namsua@users.noreply.github.com>
2021-11-29 19:01:14 +0200

    javascripti kommentaarid

--- Avaleht/avaleht.js
+++ Avaleht/avaleht.js
@@ -1,14 +1,14 @@
-var slideIndex = 0;
-showSlides();
+var slideIndex = 0;   // määrab ära milline slaid kuvatakse esimesena
+showSlides();   // kutsub välja showSlides funktsiooni
 
 function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("slaid");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  var slides = document.getElementsByClassName("slaid");  // loob uue muutuja ja määrab tema väärtuseks elemendid, mille nimeks on "slaid"
+  for (i = 0; i < slides.length; i++) {   
+    slides[i].style.display = "none";   // peidab ära eelmise slaidi
   }
-  slideIndex++;
+  slideIndex++;   // järgmine slaid
   if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  slides[slideIndex-1].style.display = "block"; // kuvab järgmise slaidi
+  setTimeout(showSlides, 5000); // muudab slaidi iga 5 sekundi tagant
 } 
